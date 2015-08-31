142cf2cc93d271cc656378fe84d6ceecce849cfe
student <student@ai251lab0214m11.aicala.edmc.stu>
2015-08-31 09:28:11 -0700

    first push

---
+++ php/registration.php
@@ -0,0 +1,24 @@
+<!-- registration -->
+
+<section id="registration">
+	<!-- lazy registration form -->
+	<form id="lazy-registration" method="post" action="#">
+		
+		<!-- fieldset name and email -->
+		<fieldset>
+			<label for="lazy-name">Lazy Name:</label>
+				<input type="text" name="lazy-name" id="lazy-name" value="">
+			<label for="lazy-email">Lazy Email:</label>
+				<input type="text" name="lazy-email" id="lazy-email" value="">
+		</fieldset> <!-- end fieldset -->
+
+		<!-- fieldset name and email -->
+		<fieldset>
+			<label for="lazy-password">Password</label>
+				<input type="text" name="lazy-password" id="lazy-password" value="">
+			<label for="lazy-password-conf">Confirm Password:</label>
+				<input type="text" name="lazy-password-conf" id="lazy-password-conf" value="">
+		</fieldset> <!-- end fieldset -->
+
+	</form> <!-- end lazy registration form -->
+</section>
